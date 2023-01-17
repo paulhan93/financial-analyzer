f9f767482b191f6f868fb020e69f3a2969ce9eb6
Paul Han <paul.han0822@gmail.com>
2023-01-17 00:20:21 -0800

    add code template

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+class Company:
+    def __init__(self, name):
+        # constructor, initialize class variables
+        self.name = name
+    
+    def method1(self):
+        # method 1, add some functionality
+        pass
+        
+    def method2(self):
+        # method 2, add some functionality
+        pass
+        
+    def __str__(self):
+        # return string representation of the class
+        return f"Company object with name: {self.name}"
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
