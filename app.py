df02c34e219fc54a44c560cf915f42e93db21c96
Paul Han <paul.han0822@gmail.com>
2023-01-17 19:51:53 -0800

    set up sqlite database

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+"""
+Project Name: Financial Analyzer
+Description : This application will output the confidence level of investments by conducting ratio analysis of a given company.
+Author      : Paul (Ki Tae) Han
+License     : MIT license
+""" 
+
+# import modules
+import sqlite3
 import numpy as np
 import pandas as pd
 
+# classes
 class Company:
     def __init__(self, name, current_assets, current_liabilities, inventory):
         # constructor, initialize class variables
@@ -20,7 +30,7 @@ class Company:
     def quick_ratio(self):
         """
         Calculate the quick ratio; (current assets - inventory) / current liabilities \n
-        Quick ratio is considered to be a more conservative measure of a company’s liquidity 
+        Quick ratio is considered to be a more conservative measure of a company's liquidity 
         than the current ratio. A ratio of 1 or higher is generally considered healthy.
         """
         return (self.current_assets - self.inventory) / self.current_liabilities
@@ -34,7 +44,30 @@ class Company:
                 Quick ratio     : {self.quick_ratio()}
                 """
 
+# functions
+def connect_to_database():
+    # connect to the database
+    conn = sqlite3.connect('financial_analyzer.db')
+
+    # create a cursor object to execute SQL commands
+    cursor = conn.cursor()
+    #cursor.execute('''CREATE TABLE aapl (year INTEGER PRIMARY KEY, currentAssets REAL, currentLiabilities REAL, inventory REAL)''')
+    #cursor.execute("INSERT INTO aapl (year, currentAssets, currentLiabilities, inventory) VALUES (2022, 135405, 153982, 4946)")
+    cursor.execute("SELECT * FROM aapl")
+
+    # fetch all the rows from the result of a query
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # commit to save the changes and close connection to database
+    conn.commit()
+    conn.close()
+
+
 def main():
+    # connect to the sqlite database
+    connect_to_database()
     company = Company("AAPL", 135405, 153982, 4946)
     print(company)
 
