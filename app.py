6cd73643e82b56cae8c8c00c7f3ebd361e97f743
Paul Han <paul.han0822@gmail.com>
2023-01-17 18:49:28 -0800

    add current and quick ratio methods

--- app.py
+++ app.py
@@ -2,25 +2,41 @@ import numpy as np
 import pandas as pd
 
 class Company:
-    def __init__(self, name):
+    def __init__(self, name, current_assets, current_liabilities, inventory):
         # constructor, initialize class variables
         self.name = name
-    
-    def method1(self):
-        # method 1, add some functionality
-        pass
+        self.current_assets = current_assets
+        self.current_liabilities = current_liabilities
+        self.inventory = inventory
+
+    def current_ratio(self):
+        """
+        Calculate the current ratio; current assets / current liabilities \n
+        Current ratio is a measure of a company's ability to meet short-term obligations. 
+        A ratio of 1 or higher is generally considered healthy.
+        """
+        return self.current_assets / self.current_liabilities
         
-    def method2(self):
-        # method 2, add some functionality
-        pass
-        
+    def quick_ratio(self):
+        """
+        Calculate the quick ratio; (current assets - inventory) / current liabilities \n
+        Quick ratio is considered to be a more conservative measure of a company’s liquidity 
+        than the current ratio. A ratio of 1 or higher is generally considered healthy.
+        """
+        return (self.current_assets - self.inventory) / self.current_liabilities
+
     def __str__(self):
         # return string representation of the class
-        return f"Company object with name: {self.name}"
-
+        return f"""
+                Company object with... 
+                Name            : {self.name}
+                Current ratio   : {self.current_ratio()}
+                Quick ratio     : {self.quick_ratio()}
+                """
 
 def main():
-    pass
+    company = Company("AAPL", 135405, 153982, 4946)
+    print(company)
 
 if __name__ == "__main__":
     main()
