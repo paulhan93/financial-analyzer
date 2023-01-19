909b589f6916a73b958fc21672d5205548dd0517
Paul Han <paul.han0822@gmail.com>
2023-01-18 22:48:12 -0800

    add apple financial data into database

--- app.py
+++ app.py
@@ -9,45 +9,15 @@ License     : MIT license
 import sqlite3
 import numpy as np
 import pandas as pd
-
-# classes
-class Company:
-    def __init__(self, name, current_assets, current_liabilities, inventory):
-        # constructor, initialize class variables
-        self.name = name
-        self.current_assets = current_assets
-        self.current_liabilities = current_liabilities
-        self.inventory = inventory
-
-    def current_ratio(self):
-        """
-        Calculate the current ratio; current assets / current liabilities \n
-        Current ratio is a measure of a company's ability to meet short-term obligations. 
-        A ratio of 1 or higher is generally considered healthy.
-        """
-        return self.current_assets / self.current_liabilities
-        
-    def quick_ratio(self):
-        """
-        Calculate the quick ratio; (current assets - inventory) / current liabilities \n
-        Quick ratio is considered to be a more conservative measure of a company's liquidity 
-        than the current ratio. A ratio of 1 or higher is generally considered healthy.
-        """
-        return (self.current_assets - self.inventory) / self.current_liabilities
-
-    def __str__(self):
-        # return string representation of the class
-        return f"""
-                Company object with... 
-                Name            : {self.name}
-                Current ratio   : {self.current_ratio()}
-                Quick ratio     : {self.quick_ratio()}
-                """
+from Company import Company
 
 # functions
 def connect_to_database():
     # connect to the database
-    conn = sqlite3.connect('financial_analyzer.db')
+    conn = sqlite3.connect('financial_data.db')
+
+    df = pd.read_excel("financial_data.xlsx")
+    df.to_sql('aapl', conn, if_exists='replace')
 
     # create a cursor object to execute SQL commands
     cursor = conn.cursor()
@@ -68,8 +38,12 @@ def connect_to_database():
 def main():
     # connect to the sqlite database
     connect_to_database()
-    company = Company("AAPL", 135405, 153982, 4946)
-    print(company)
+
+    #company = Company("AAPL", 135405, 153982, 4946)
+    #print(company)
+
+    #df = pd.read_excel("financial_data.xlsx")
+    #print(df.head())
 
 if __name__ == "__main__":
     main()
